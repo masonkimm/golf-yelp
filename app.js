7838ee03c0ae07f05e84d3cb84697cb3cd384edd
SungJin Kim <sungjinkimm@gmail.com>
2021-04-29 13:56:27 -0400

    mongoDB connected

--- app.js
+++ app.js
@@ -1,7 +1,20 @@
 const express = require('express');
-
-const app = express();
 const path = require('path');
+const mongoose = require('mongoose');
+const Course = require('./models/course');
+
+mongoose.connect('mongodb://localhost:27017/golf-yelp', {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error: '));
+db.once('open', () => {
+  console.log('Database connected');
+});
+const app = express();
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
@@ -9,6 +22,15 @@ app.set('views', path.join(__dirname, 'views'));
 app.get('/', (req, res) => {
   // res.send('Hello Golf World');
   res.render('home');
+});
+
+app.get('/createcourse', async (req, res) => {
+  const course = new Course({
+    title: 'VGC',
+    description: 'close by',
+  });
+  await course.save();
+  res.send(course);
 });
 
 app.listen(3000, () => {
