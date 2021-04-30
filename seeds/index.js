02016a433cdef7874ae43a3fd3e9ef6877df145a
SungJin Kim <sungjinkimm@gmail.com>
2021-04-30 10:08:35 -0400

    seeded db with placeholder

---
+++ seeds/index.js
@@ -0,0 +1,40 @@
+const mongoose = require('mongoose');
+const Course = require('../models/course');
+const cities = require('./cities');
+const { places, descriptors } = require('./seedHelpers');
+
+mongoose.connect('mongodb://localhost:27017/golf-yelp', {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error: '));
+db.once('open', () => {
+  console.log('Database connected');
+});
+
+const sample = (array) => {
+  return array[Math.floor(Math.random() * array.length)];
+};
+
+const seedDB = async () => {
+  await Course.deleteMany({});
+  // const c = new Course({ title: 'Pebble Beach' });
+  // await c.save();
+
+  for (let i = 0; i < 50; i++) {
+    const random1000 = Math.floor(Math.random() * 1000);
+    const course = new Course({
+      location: `${cities[random1000].city}, ${cities[random1000].state}`,
+      title: `${sample(descriptors)}, ${sample(places)}`,
+    });
+    await course.save();
+  }
+};
+
+seedDB().then(() => {
+  console.log('Database disconneted');
+  mongoose.connection.close();
+});
