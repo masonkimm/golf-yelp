d06d6222021e2844b4c530578ecb5cc658088640
SungJin Kim <sungjinkimm@gmail.com>
2021-05-03 16:11:57 -0400

    added ejs-mate (latout system like react.js)

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const path = require('path');
 const mongoose = require('mongoose');
 const Course = require('./models/course');
 const methodOverride = require('method-override');
+const ejsMate = require('ejs-mate');
 
 mongoose.connect('mongodb://localhost:27017/golf-yelp', {
   useNewUrlParser: true,
@@ -17,7 +18,9 @@ db.once('open', () => {
 });
 const app = express();
 
+app.engine('ejs', ejsMate);
 app.set('view engine', 'ejs');
+
 app.set('views', path.join(__dirname, 'views'));
 app.use(express.urlencoded({ exteded: true }));
 app.use(methodOverride('_method'));
