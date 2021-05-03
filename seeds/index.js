ca14d78830f4fa5c81a799cf36f70f129cc3bb79
SungJin Kim <sungjinkimm@gmail.com>
2021-05-03 17:15:04 -0400

    images added

--- seeds/index.js
+++ seeds/index.js
@@ -26,15 +26,20 @@ const seedDB = async () => {
 
   for (let i = 0; i < 50; i++) {
     const random1000 = Math.floor(Math.random() * 1000);
+    const price = Math.floor(Math.random() * 20) + 10;
     const course = new Course({
       location: `${cities[random1000].city}, ${cities[random1000].state}`,
       title: `${sample(descriptors)} ${sample(places)}`,
+      image: 'https://source.unsplash.com/collection/10734239/1200x600',
+      description:
+        'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nam magni, maxime reiciendis ut beatae accusantium soluta adipisci cumque alias eaque laboriosam consectetur quidem impedit libero eos accusamus quas odio quibusdam.',
+      price: price,
     });
     await course.save();
   }
 };
 
 seedDB().then(() => {
-  console.log('Database disconneted');
+  console.log('New data seeded successfully! Disconnecting Database');
   mongoose.connection.close();
 });
