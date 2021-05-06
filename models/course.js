86a28774d0c5c138a47c797afbbc21ea27b77663
SungJin Kim <sungjinkimm@gmail.com>
2021-05-06 17:06:45 -0400

    delete reviews middleware

--- models/course.js
+++ models/course.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const Review = require('./review');
 
 const CourseSchema = new Schema({
   title: String,
@@ -13,6 +14,17 @@ const CourseSchema = new Schema({
       ref: 'Review',
     },
   ],
+});
+
+CourseSchema.post('findOneAndDelete', async function (doc) {
+  // console.log(doc);
+  if (doc) {
+    await Review.deleteMany({
+      _id: {
+        $in: doc.reviews,
+      },
+    });
+  }
 });
 
 module.exports = mongoose.model('Course', CourseSchema);
