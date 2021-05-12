1742e599d8586057d267aa3a50d55468f4fb3ac4
SungJin Kim <sungjinkimm@gmail.com>
2021-05-11 22:56:51 -0400

    fixed extendedurl typo

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ app.engine('ejs', ejsMate);
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
-app.use(express.urlencoded({ exteded: true }));
+app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
 app.use(express.static(path.join(__dirname, 'public')));
 
