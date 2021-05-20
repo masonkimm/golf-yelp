07e7d66292c9733ffbaf7fcf67a9c94950b1f7f1
SungJin Kim <sungjinkimm@gmail.com>
2021-05-19 21:35:41 -0400

    thumbnail added for removing images in edit

--- models/course.js
+++ models/course.js
@@ -2,17 +2,21 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 const Review = require('./review');
 
+const ImageSchema = new Schema({
+  url: String,
+  filename: String,
+});
+
+ImageSchema.virtual('thumbnail').get(function () {
+  return this.url.replace('/upload', '/upload/w_200');
+});
+
 const CourseSchema = new Schema({
   title: String,
   price: Number,
   description: String,
   location: String,
-  images: [
-    {
-      url: String,
-      filename: String,
-    },
-  ],
+  images: [ImageSchema],
   author: {
     type: Schema.Types.ObjectId,
     ref: 'User',
